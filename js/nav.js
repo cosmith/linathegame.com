d45c331e9698fbb2ee7a36dd78c1e690d4d336a0
Gabriel Samain <gabriel.samain@gmail.com>
2014-05-14 19:40:44 -0700

    addding sweet carousel for screenshots

--- js/nav.js
+++ js/nav.js
@@ -5,8 +5,17 @@
 
 
 $(document).ready(function() {
+	// Init nav menu on the right
 	$("#nav-side li").click(function() {
 		var id = $(this).find("a").attr("id");
 		$("body").scrollTo(id, 	200);
-	})
+	});
+
+	// Init slick carousel
+	$('.screenshots').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+	});
 })
