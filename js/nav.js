4705de382a417b021a2054701511921ec9382558
Corentin Smith <corentin.smith@gmail.com>
2015-01-25 13:42:55 +0100

    Update images

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,6 @@
 /*!
  * JS related to site navigation
- * Licensed under Chybre&Co ® 
+ * Licensed under Chybre&Co ®
  */
 
 
@@ -8,12 +8,12 @@ $(document).ready(function() {
 	// Init nav menu on the right
 	$("#nav-side li").click(function() {
 		var id = $(this).find("a").attr("id");
-		$("body").scrollTo(id, 	200);
+		$("body").scrollTo(id, 200);
 	});
 
 	// Init slick carousel
 	$('.screenshots').slick({
-		slidesToShow: 3,
+		slidesToShow: 2,
 		slidesToScroll: 1,
 		autoplay: true,
 		autoplaySpeed: 2000,
